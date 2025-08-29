2cab6212bab51cd8feb6e76ea13322820888356f
simonpunk <simonpunk2016@gmail.com>
2025-08-29 18:10:26 -0300

    fs: susfs: Make sus_mounts invisible only for apps that have sus_mounts umounted - There are two situations only now:   1. if "ksu_susfs hide_sus_mnts_for_all_procs 1" is set(default 1 in kernel), all processes will NOT see any sus mounts in mountinfo   2. if "ksu_susfs hide_sus_mnts_for_all_procs 0" is set, all processes that have NO sus_mounts umounted can still see the sus_mounts, and vice versa, processes that have sus_mounts umounted will NOT see any sus_mounts.
    - Make sure "ksu_susfs hide_sus_mnts_for_all_procs 0" is executed only after post-fs-data.sh.

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -106,7 +106,11 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+<<<<<<< HEAD
 	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID))
+=======
+	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID)
+>>>>>>> 8c84e19dca61 (fs: susfs: Make sus_mounts invisible only for apps that have sus_mounts umounted)
 		return 0;
 #endif
 
@@ -147,7 +151,7 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
-	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID))
+	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID)
 		return 0;
 #endif
 
@@ -216,7 +220,11 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 	int err;
 
 #ifdef CONFIG_KSU_SUSFS_SUS_MOUNT
+<<<<<<< HEAD
 	if (unlikely(r->mnt_id >= DEFAULT_SUS_MNT_ID))
+=======
+	if (susfs_hide_sus_mnts_for_all_procs && r->mnt_id >= DEFAULT_SUS_MNT_ID)
+>>>>>>> 8c84e19dca61 (fs: susfs: Make sus_mounts invisible only for apps that have sus_mounts umounted)
 		return 0;
 #endif
 
